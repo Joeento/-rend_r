13f71ab19cf40659935481a31f3ed3057d27c0c9
Eric Kudler <joeento@gmail.com>
2015-10-29 16:53:25 -0400

    Added some simple layout

---
+++ src/RendR/PrintR.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RendR;
+
+class PrintR
+{
+    public static function r($input)
+    {
+    	//return '<pre>'.print_r($input, true).'</pre>';
+    	switch (gettype($input)) {
+    		case 'object':
+    		case 'array':
+	    		$output .= '<ul>';
+		    	foreach($input as $key => $value) {
+		    		$output .= '<li>'.$key.' - '.self::r($value).'</li>';
+		    	}
+		    	$output .= '</ul>';
+		    	return $output;
+		    break;
+		    default:
+		    	return $input;
+
+    	}
+    }
+}
--- tests/print_r.php
+++ tests/print_r.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
-use RendR\SayHello;
+use RendR\PrintR;
 
-echo SayHello::world();
+$json = file_get_contents('https://www.govtrack.us/api/v2/role?current=true');
+$obj = json_decode($json);
+echo Printr::r($obj);
